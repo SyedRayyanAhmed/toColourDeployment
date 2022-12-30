e7571c1c5781b1269c047dfe28efdc1b44ed6a22
SyedRayyanAhmed <SyedRayyanAhmed@users.noreply.github.com>
2022-12-31 02:03:28 +0530

    change 1

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 
 echo " BUILD START"
-python3.8 pip install -r requirements.txt
-#Python3.8 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+Python3.8 manage.py collectstatic
 echo " BUILD END"
