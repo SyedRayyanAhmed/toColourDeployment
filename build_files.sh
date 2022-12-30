a599e65ea8723d24ce7385602b5b55b8dcc46901
SyedRayyanAhmed <SyedRayyanAhmed@users.noreply.github.com>
2022-12-31 01:18:16 +0530

    python v 3.9

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 
 echo " BUILD START"
-python3.8 pip install -r requirements.txt
-Python3.8 manage.py collectstatic --noinput --clear
+python3.9 pip install -r requirements.txt
+Python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
