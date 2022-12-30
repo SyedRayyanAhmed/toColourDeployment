fd7254cce1075f28a99e49034662b184983a570d
SyedRayyanAhmed <SyedRayyanAhmed@users.noreply.github.com>
2022-12-31 01:12:57 +0530

    change on build_sh py version

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 
 echo " BUILD START"
-python 3.8 pip install -r requirements.txt
-Python 3.8 manage.py collectstatic --noinput --clear
+python3.8 pip install -r requirements.txt
+Python3.8 manage.py collectstatic --noinput --clear
 echo " BUILD END"
