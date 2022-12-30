42837f7c51dc225a47e74a9e3c5b5825719daf86
SyedRayyanAhmed <SyedRayyanAhmed@users.noreply.github.com>
2022-12-31 01:45:47 +0530

    many changes

--- toColourWebApp/urls.py
+++ toColourWebApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from toColourDeployment import settings
 
 urlpatterns = [
     path('', views.Welcome),
     path('exe_toColour', views.Execute_toColour, name = 'exe_toColour'),
     ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
